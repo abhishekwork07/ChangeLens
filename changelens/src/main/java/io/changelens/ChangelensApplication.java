e0b107f4feecc2e4ec4e3eaafca62a4be0aa9e1a
Abhishek Gupta <abhishek.work07@gmail.com>
2026-08-13 21:05:52 +0530

    ChangeLens Foundation

---
+++ changelens/src/main/java/io/changelens/ChangelensApplication.java
@@ -0,0 +1,13 @@
+package io.changelens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangelensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangelensApplication.class, args);
+	}
+
+}
---
+++ changelens/src/test/java/io/changelens/ChangelensApplicationTests.java
@@ -0,0 +1,13 @@
+package io.changelens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangelensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
